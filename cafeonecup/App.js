26ec8c6429520024b1b4dc029bd0a8751b4eaf82
vansshoes5 <kdh5532130@gmail.com>
2021-11-22 15:08:34 +0900

    2021/11/22 App.JS
    app.js 드디어 연결함.
    메인 화면 구현 중
    사실 여태껏 많이 만졌지만, 업로드 방법을 몰랐었다,, 흑

--- cafeonecup/App.js
+++ cafeonecup/App.js
@@ -1,112 +1,141 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import { render } from '@testing-library/react';
+import React, {Component} from 'react';
+import { StyleSheet, Button, View, Text, TextInput, ScrollView } from "react-native";
+import MainInterface from './MainInterface'
+class App extends Component {
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
+  state = {
+    myTextInput:"",
+    alphabet:['a','b','c','d']
+ }
+  onChangeInput=(event)=>{
+    this.setState({
+      myTextInput:event
+    })
+  }
+  onAddTextInput=()=>{
+    this.setState(prevState=>{
+      return{
+        myTextInput:'', // reset 
+        alphabet:[...prevState.alphabet,prevState.myTextInput]
+      }
+    })
+  }
+/*module.exports ={
+    This_Test: ['./font']
+};*/
+  
+  render(){
+    return(
+      <View style={{
+          backgroundColor: '#EBD7C3',
+          height: "100%",
+          marginTop:0,
+  
+      }}>
+          <Text style={styles.title}>
+          카페 한 잔
+          </Text>
+          <Button 
+          title="로그인"
+          color='f3f3f3'
+          style={styles.LoginButton}
+          ></Button>
+          <TextInput
+              style={styles.SearchText}
+              value={this.state.myTextInput}
+              onChangeText={this.onChangeInput}
+              multiline={false}
+              editable={true}
+          ></TextInput>
+          <Button title="검색"
+          color='#4B3621'
           style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
+            width: 30
+            
+          }}
+          //onPress
+          ></Button>
+  
+      </View>
+    )
+  }
+}
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+
+
+  //각 컴포넌트들이 들어갈 컨테이너 제작하기
+  //컨테이너 1, 2, 5 (상단 두 개, 하단 1개)
+  container_01 :{
+      top: 20,
+      flex: 1,
+      marginTop: 10,
+      marginHorizontal: 16,
+      backgroundColor: '',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+
+  //카페 이미지와 라벨 들어갈 컨테이너 3, 4
+  container_02 :{
+      flex: 3,
+      marginTop: 10,
+      paddingBottom: 6,
+
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  MenuLabel :{
+      alignContent: 'flex-start',
+      textAlign: 'center',
+      marginLeft: 5,
+      backgroundColor: '#FFFFFF',
+      width: 70,
+      height: 25,
   },
-  highlight: {
-    fontWeight: '700',
+
+  //메인 타이틀 정렬하기
+  title: {
+      color: '#C2A484',
+      textAlign: 'left',
+      paddingVertical: 20,
+      //fontStyle: '',
+      fontSize: 60,
   },
-});
+
+  //로그인 버튼
+  LoginButton:{
+      width: '15%',
+      //height: 20,
+      color: 'f3f3f3',
+      paddingBottom: 5,
+      alignItems: 'center',
+  },
+
+  //카페 이미지, 카페 이름 정렬하기
+  CafeTitle:{
+      flexDirection: 'row',
+      justifyContent: 'space-around',
+  },
+  
+  //공지사항 버튼
+  AlertButton:{
+      width: 45,
+      height: 20,
+  },
+  
+  //검색창
+  SearchText:{
+      width: '75%',
+      backgroundColor: '#ffffff',
+      marginLeft: 6,
+      fontSize: 20,
+      padding: 15,
+  },
+
+  //검색 버튼
+  SearchButton:{
+      width: '23%',
+      color: '#4B3621'
+      
+  },
+})
 
 export default App;
